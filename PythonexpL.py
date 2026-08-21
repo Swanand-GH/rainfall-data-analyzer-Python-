c2d9d60b96d0f2f28be424d765610424142d099b
Swanand-GH <swanandgh@users.noreply.github.com>
2026-08-21 11:43:22 +0530

    Add rainfall data analyzer

---
+++ PythonexpL.py
@@ -0,0 +1,9 @@
+rainfall = [120, 150, 90, 200, 175]
+
+total = sum(rainfall)
+maximum = max(rainfall)
+minimum = min(rainfall)
+
+print("Total Rainfall:", total, "mm")
+print("Maximum Rainfall:", maximum, "mm")
+print("Minimum Rainfall:", minimum, "mm")
